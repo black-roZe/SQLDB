794930937aedd219bdd326977707b7047a633f17
Abhijay Tyagi <abhijay@pop-os.localdomain>
2024-08-24 17:11:23 +0530

    added functionality to count the number of tables in the DB file by looking for binary coverted CREATE TABLE statement in the while DB file

--- app/main.py
+++ app/main.py
@@ -10,11 +10,13 @@ command = sys.argv[2]
 if command == ".dbinfo":
     with open(database_file_path, "rb") as database_file:
         # You can use print statements as follows for debugging, they'll be visible when running tests.
-        print("Logs from your program will appear here!")
+        # print("Logs from your program will appear here!") 
 
         # Uncomment this to pass the first stage
         database_file.seek(16)  # Skip the first 16 bytes of the header
         page_size = int.from_bytes(database_file.read(2), byteorder="big")
+        number_of_tables = sum(db_file_line.count(b"CREATE TABLE") for db_file_line in database_file)
         print(f"database page size: {page_size}")
+        print(f"number of tables: {number_of_tables}")
 else:
     print(f"Invalid command: {command}")
